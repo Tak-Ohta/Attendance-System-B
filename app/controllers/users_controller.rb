c7d6303eeb6584efa2deead3d2b854a4f2336196
Tak-Ohta <sacredfield.tak@gmail.com>
2020-11-15 08:14:28 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
+    else
+      flash[:danger] = "新規登録に失敗しました。"
+      render :new
+    end     
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
