daccd4cf936af8575d29007aae26055136504149
Tak-Ohta <sacredfield.tak@gmail.com>
2020-11-24 12:31:52 +0000

    ユーザー情報編集機能完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,21 @@ class UsersController < ApplicationController
       render :new
     end     
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "アカウント情報を更新しました。"
+      redirect_to users_url
+    else
+      flash.now[:danger] = "更新に失敗しました。"
+      render :edit
+    end
+  end
 
 
   private
