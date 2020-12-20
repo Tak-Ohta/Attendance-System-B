e503a736b449630655e5e7836ab00a3e5f8be73d
Tak-Ohta <sacredfield.tak@gmail.com>
2020-12-20 10:37:20 +0000

    Fnish add-user-attendances

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -4,6 +4,10 @@ module AttendancesHelper
       return "出社" if attendance.started_at.nil?
       return "退社" if attendance.started_at.present? && attendance.finished_at.nil?
     end
+    false
   end
-  false
+  
+  def working_times(start, finish)
+    format("%.2f", ((finish - start) / 3600.0))
+  end
 end
