7b3dacdffb0cccba6f4ebedf403c5181c0ade1ff
Tak-Ohta <sacredfield.tak@gmail.com>
2020-12-26 07:17:43 +0000

    勤怠編集ページ作成前まで完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :index
@@ -42,11 +42,28 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+  
+  def edit_basic_info
+  end
+  
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "基本情報を更新しました。"
+      redirect_to users_url
+    else
+      flash[:danger] = "更新に失敗しました。やり直してください。"
+      render 'edit_basic_info'
+    end   
+  end
 
 
   private
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:basic_time, :work_time)
     end
     
     def set_user
